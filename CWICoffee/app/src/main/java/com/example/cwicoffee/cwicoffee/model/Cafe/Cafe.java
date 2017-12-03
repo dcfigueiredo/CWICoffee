15929b75a22284f8aaad9378681cf3d2d8c0fc26
dcfigueiredo <daniel.carvalho.figueiredo@gmail.com>
2017-12-03 18:28:19 -0200

    auth

--- CWICoffee/app/src/main/java/com/example/cwicoffee/cwicoffee/model/Cafe/Cafe.java
+++ CWICoffee/app/src/main/java/com/example/cwicoffee/cwicoffee/model/Cafe/Cafe.java
@@ -4,6 +4,7 @@ import com.example.cwicoffee.cwicoffee.model.CafezeiroCafe.CafezeiroCafe;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Created by daniel.figueiredo on 25/11/2017.
@@ -12,8 +13,13 @@ import java.time.LocalDate;
 public class Cafe {
 
     private Long idCafe;
+
     private LocalDate dtCafe;
+
     private List<CafezeiroCafe> listCafezeiroCafe;
+
     private BigDecimal vlGasto;
+
     private StatusCafe status;
+
 }
